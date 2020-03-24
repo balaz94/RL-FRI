7d23edc7f380949e823e4d180f2f73264c695bf6
balaz42 <balaz.marek1994@gmail.com>
2020-03-24 10:59:35 +0100

    master

--- agents/ppo.py
+++ agents/ppo.py
@@ -1,3 +1,4 @@
+import copy
 import numpy as np
 import torch
 import torch.nn as nn
@@ -5,15 +6,17 @@ import torch.nn.functional as F
 import torch.optim as optim
 from utils.stat import write_to_file
 
-class AgentA2C:
-    def __init__(self, gamma, actions_count, model, lr = 0.0001, beta_entropy = 0.001, value_loss_coef = 0.5, id=1, name='a2c'):
+class AgentPPO:
+    def __init__(self, gamma, actions_count, model, lr = 0.0001, beta_entropy = 0.001, value_loss_coef = 0.5, id=1, name='ppo', epsilon = 0.2):
         self.gamma = gamma
         self.actions_count = actions_count
         self.model = model
+        self.old_model = copy.deepcopy(model)
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         print('device: ', self.device)
         self.model.to(self.device)
+        self.old_model.to(self.device)
         self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
 
         self.beta_entropy = beta_entropy
@@ -21,6 +24,8 @@ class AgentA2C:
 
         self.average_score = []
         self.episodes = 0
+        self.upper_bound = 1 + epsilon
+        self.lower_bound = 1 - epsilon
 
         self.id = id
         self.name = name
@@ -51,6 +56,7 @@ class AgentA2C:
         for iteration in range(start_iteration, max_iteration):
             mem_values = []
             mem_log_probs = []
+            mem_old_log_probs = []
             mem_entropies = []
 
             mem_rewards = []
@@ -61,15 +67,24 @@ class AgentA2C:
                 logits, values = logits.cpu(), values.cpu()
 
                 probs = F.softmax(logits, dim=-1)
+
                 log_probs = F.log_softmax(logits, dim=-1)
+                old_log_probs = F.log_softmax(logits_old, dim=-1).detach()
                 entropies = (log_probs * probs).sum(1, keepdim=True)
 
                 actions = probs.multinomial(num_samples=1).detach()
-                log_probs_policy = log_probs.gather(1, actions)
+
+                log_policy = log_probs.gather(1, actions)
+
+                with torch.no_grad():
+                    logits_old, _ = self.old_model(observations)
+                    logits_old = logits_old.cpu()
+                    old_log_policy = old_log_probs.gather(1, actions).detach()
 
                 mem_values.append(values)
                 mem_entropies.append(entropies)
-                mem_log_probs.append(log_probs_policy)
+                mem_log_probs.append(log_policy)
+                mem_old_log_probs.append(old_log_policy)
 
                 rewards = torch.zeros([len_workers, 1])
                 non_terminals = torch.ones([len_workers, 1], dtype=torch.int8)
@@ -97,10 +112,19 @@ class AgentA2C:
                 R = mem_rewards[step] + self.gamma * R * mem_non_terminals[step]
                 advantage = R - mem_values[step]
                 value_loss += advantage**2
-                policy_loss += - mem_log_probs[step] * advantage.detach() + self.beta_entropy * mem_entropies[step]
+                advantage_detach = advantage.detach()
+
+                ratio = torch.exp(mem_log_probs[step] - mem_old_log_probs[step])
+                surr_policy = ratio * advantage_detach
+                lower_bound = self.lower_bound * advantage_detach
+                upper_bound = self.upper_bound * advantage_detach
+                surr_clip = torch.max(lower_bound, upper_bound)
+                policy_loss += - torch.min(surr_policy, surr_clip) + self.beta_entropy * mem_entropies[step]
 
             policy_loss = policy_loss / max_steps
             value_loss = value_loss / max_steps
+
+            self.old_model.load_state_dict(self.model.state_dict())
 
             self.optimizer.zero_grad()
             loss = policy_loss.mean() + self.value_loss_coef * value_loss.mean()
@@ -120,14 +144,15 @@ class AgentA2C:
 
                     if iteration % 10000 == 0:
                         self.average_score = self.average_score[-100:]
-                        write_to_file(text, 'logs/' + self.name + '_' + str(self.id) + '_' + str(iteration) + '_a2c.txt')
-                        torch.save(self.model.state_dict(), 'trained_models/' + self.name + '_' + str(self.id) + '_' + str(iteration) + '_a2c.pt')
+                        write_to_file(text, 'logs/' + self.name + '_' + str(self.id) + '_' + str(iteration) + '_ppo.txt')
+                        torch.save(self.model.state_dict(), 'trained_models/' + self.name + '_' + str(self.id) + '_' + str(iteration) + '_ppo.pt')
 
     def load_model(self):
         self.model.load_state_dict(torch.load('trained_models/' + self.name))
+        self.old_model = copy.deepcopy(self.model)
 
     def save_model(self):
-        torch.save(self.model.state_dict(), 'trained_models/' + self.name + '_' + str(self.id) + '_a2c.pt')
+        torch.save(self.model.state_dict(), 'trained_models/' + self.name + '_' + str(self.id) + '_ppo.pt')
 
 def reward_func(r):
     return r
